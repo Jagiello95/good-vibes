49117a016a12a7841835825c5ecb08135901aec4
Andrzej <and.jagiello@gmail.com>
2024-09-29 03:24:55 +0200

    good-vibes rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'hackyeah2024';
+  title = 'good-vibes';
 
   constructor(private api: ApiService) {}
 
